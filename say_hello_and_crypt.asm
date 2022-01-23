af403199ee709c75d689292b1164c3180ba16448
lfontesm <moreirafontesleo@gmail.com>
2022-01-23 19:16:40 -0300

    (ugly code) trying some things with peb

--- say_hello_and_crypt.asm
+++ say_hello_and_crypt.asm
@@ -20,6 +20,7 @@ crypt:
         jmp crypt
 
 exit:
+        call get_peb
         push 0
         call [ExitProcess]
 
@@ -37,6 +38,51 @@ bux:
         call [MessageBox]
         ret
 
+get_peb:
+        xor eax, eax
+        mov eax, [fs:0x30]
+        mov eax, [eax + 0xc]
+        mov esi, [eax + 0x14]
+        lodsd
+        xchg eax, esi
+        lodsd
+        xchg eax, esi
+        lodsd
+        xchg eax, esi
+        lodsd
+        mov eax, [eax + 0x10]
+
+find_export_dir:
+        .e_lfanew   = 3Ch
+        .data_dir_0 = 78h
+
+        mov ecx, [eax + .e_lfanew]
+        mov ecx, [eax + ecx + .data_dir_0]
+        add ecx, eax
+
+read_export_dir:
+        .export_names_num  = 18h
+        .export_funcs_addr = 1Ch
+        .export_names_addr = 20h
+        .export_ords_addr  = 24h
+
+        ; Obtain info from IMAGE_EXPORT_DIRECTORY
+        mov edx, [ecx + .export_funcs_addr] ; AddressOfFunctions
+        mov [addrOfFunc], edx
+
+        mov edx, [ecx + .export_names_addr] ; AddressOfNames
+        mov [addrOfName], edx
+
+        mov edx, [ecx + .export_ords_addr]  ; AddressOfNameOrdinals
+        mov [addrOfOrdi], edx
+
+        mov edx, [ecx + .export_names_num]  ; NumberOfNames
+        mov [addrOfNumb], edx
+
+        add [addrOfFunc], ecx
+        add [addrOfName], ecx
+        add [addrOfOrdi], ecx
+
 section '.idata' import data readable
 
  library user, 'USER32.DLL',\
@@ -47,3 +93,11 @@ section '.idata' import data readable
 
  import kernel, \
         ExitProcess, 'ExitProcess'
+
+section '.bss' readable writable
+
+ msgbox dd ?
+ addrOfFunc dd ?
+ addrOfName dd ?
+ addrOfOrdi dd ?
+ addrOfNumb dd ?
